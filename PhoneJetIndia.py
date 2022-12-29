655416c7a99d4da9f957f8cb2be22670a8888674
TnYtCoder <115485810+TnYtCoder@users.noreply.github.com>
2022-12-29 20:54:16 +0530

    Update PhoneJetIndia.py

--- PhoneJetIndia.py
+++ PhoneJetIndia.py
@@ -11,7 +11,7 @@ x = "Your Number Is +91"
 a = (random.randrange(7896253429, 9876865518))
 
 print(x, a)
-print("")
-print("Mostly The which starts from 8 wil work nicely 😉")
+print("Some Of The Number Will Not Work !!")
+print("Mostly The which starts from 8 will work nicely 😉")
 print("")
 print("I am not responsible if you use this in wrong way !!! ")
