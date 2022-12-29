7171d393fbc8f20b034c669303a192d7622e41de
TnYtCoder <115485810+TnYtCoder@users.noreply.github.com>
2022-12-29 12:27:55 +0530

    PhoneJetIndia.py

---
+++ PhoneJetIndia.py
@@ -0,0 +1,16 @@
+import random
+print("")
+print("𝚃𝚗𝚈𝚝𝙲𝚘𝚍𝚎𝚛")
+print("")
+
+print("")
+print("Please Don't Number For Any Test !!!")
+print("")
+x = "Your Random Number Is +91" 
+
+a = (random.randrange(7896253429, 9876865518))
+
+print(x, a)
+
+print("")
+print("I am not responsible if you use this in wrong way !!! ")
